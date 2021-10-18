3b381a66ddaf4225d314be14dc747770b334af6c
Arun Subramaniam <s.arunsubramaniam@gmail.com>
2021-10-19 01:21:48 +0530

    portfolio has been updated

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -25,6 +25,27 @@ $(document).ready(function () {
     return array;
   }
 
+	
+// Code By Webdevtrick ( https://webdevtrick.com )
+var timelineSwiper = new Swiper ('.timeline .swiper-container', {
+  direction: 'vertical',
+  loop: false,
+  speed: 1600,
+  pagination: '.swiper-pagination',
+  paginationBulletRender: function (swiper, index, className) {
+    var year = document.querySelectorAll('.swiper-slide')[index].getAttribute('data-year');
+    return '<span class="' + className + '">' + year + '</span>';
+  },
+  paginationClickable: true,
+  nextButton: '.swiper-button-next',
+  prevButton: '.swiper-button-prev',
+  breakpoints: {
+    768: {
+      direction: 'horizontal',
+    }
+  }
+});
+
   $.getJSON("assets/js/data.json", function (data) {
     // shuffle(data);
     Object.entries(data).forEach(([key, value]) => {
